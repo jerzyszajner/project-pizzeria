c212dc166ea8f1c4abecaa5d3ad59b740a3c1cd3
jerzy szajner <jerzy.szajner@gmail.com>
2023-12-20 17:24:18 +0100

    feat(booking-logic): Implement Class Inheritance and Integrate External Plugins - Established a BaseWidget class to serve as a foundation for various interactive widgets within the application, leveraging class inheritance. - Developed DatePicker and HourPicker classes extending from BaseWidget, tailored to handle date and time inputs for the booking feature. - Integrated third-party plugins 'flatpickr' and 'rangeSlider' to enhance the date and time input fields with user-friendly interfaces. - Adapted 'Booking.js' to utilize the newly created widget classes, ensuring a consistent and efficient user experience. - Configured 'index.html' to load the necessary scripts and styles for the plugins, maintaining a clean and organized codebase.
    Impact: These enhancements significantly improve the usability of the Booking page, offering users a seamless way to select dates and times for their reservations. The use of class inheritance and external plugins demonstrates the application's flexibility and the commitment to modern, scalable development practices.

--- dist/js/components/AmountWidget.js
+++ dist/js/components/AmountWidget.js
@@ -1,67 +1,60 @@
 import { settings, select } from '../settings.js';
+import BaseWidget from './BaseWidget.js';
 
-class AmountWidget {
+class AmountWidget extends BaseWidget{
   constructor(element) {
-    const thisWidget = this;
+    super(element, settings.amountWidget.defaultValue);
 
-    //console.log('AmountWidget:', thisWidget);
-    //console.log('constructor arguments:', element);
+    const thisWidget = this;
 
     thisWidget.getElements(element);
+
     thisWidget.initActions();
+
     // Check if input value is provided; if not, use default value
-    thisWidget.setValue(thisWidget.input.value || settings.amountWidget.defaultValue);
+    thisWidget.setValue(thisWidget.dom.input.value || settings.amountWidget.defaultValue);
+
+    // console.log('AmountWidget:', thisWidget);
+    // console.log('constructor arguments:', element);
   }
 
-  getElements(element) {
+  getElements() {
     const thisWidget = this;
 
-    thisWidget.element = element;
-    thisWidget.input = thisWidget.element.querySelector(select.widgets.amount.input);
-    thisWidget.linkDecrease = thisWidget.element.querySelector(select.widgets.amount.linkDecrease);
-    thisWidget.linkIncrease = thisWidget.element.querySelector(select.widgets.amount.linkIncrease);
+    thisWidget.dom.input = thisWidget.dom.wrapper.querySelector(select.widgets.amount.input);
+    thisWidget.dom.linkDecrease = thisWidget.dom.wrapper.querySelector(select.widgets.amount.linkDecrease);
+    thisWidget.dom.linkIncrease = thisWidget.dom.wrapper.querySelector(select.widgets.amount.linkIncrease);
+  }
+
+  isValid(value){
+    return !isNaN(value) 
+      && value >= settings.amountWidget.defaultMin 
+      && value <= settings.amountWidget.defaultMax;
+  }
+  
+  renderValue(){
+    const thisWidget = this;
+
+    thisWidget.dom.input.value = thisWidget.value;
   }
 
   initActions() {
     const thisWidget = this;
 
-    thisWidget.input.addEventListener('change', function () {
-      thisWidget.setValue(thisWidget.input.value);
+    thisWidget.dom.input.addEventListener('change', function () {
+    // thisWidget.setValue(thisWidget.dom.input.value);
+      thisWidget.value = thisWidget.dom.input.value;
     });
 
-    thisWidget.linkDecrease.addEventListener('click', function (event) {
+    thisWidget.dom.linkDecrease.addEventListener('click', function (event) {
       event.preventDefault();
       thisWidget.setValue(thisWidget.value - 1);
     });
 
-    thisWidget.linkIncrease.addEventListener('click', function (event) {
+    thisWidget.dom.linkIncrease.addEventListener('click', function (event) {
       event.preventDefault();
       thisWidget.setValue(thisWidget.value + 1);
     });
-  }
-
-  setValue(value) {
-    const thisWidget = this;
-
-    const newValue = parseInt(value);
-
-    /* TODO: Add validation */
-    if (newValue >= settings.amountWidget.defaultMin && newValue <= settings.amountWidget.defaultMax) {
-      if (thisWidget.value !== newValue && !isNaN(newValue)) {
-        thisWidget.value = newValue;
-        thisWidget.input.value = thisWidget.value;
-        thisWidget.announce();
-      }
-    }
-  }
-
-  announce() {
-    const thisWidget = this;
-
-    const event = new CustomEvent('updated', {
-      bubbles: true
-    });
-    thisWidget.element.dispatchEvent(event);
   }
 }
 
--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,5 +1,7 @@
 import { templates, select } from '../settings.js';
 import AmountWidget from './AmountWidget.js';
+import DatePicker from './DatePicker.js';
+import HourPicker from './HourPicker.js';
 
 class Booking {
   constructor(element) {
@@ -14,12 +16,16 @@ class Booking {
     const thisBooking = this;
 
     const generatedHTML = templates.bookingWidget();
+    
     thisBooking.dom = {}; // Create an empty object thisBooking.dom
     thisBooking.dom.wrapper = thisBooking.element; // Assign a reference to the container
     thisBooking.dom.wrapper.innerHTML = generatedHTML; // Change the wrapper content
-    // Dodawanie referencji do inputów
+    // Adding references to inputs
     thisBooking.dom.peopleAmount = thisBooking.dom.wrapper.querySelector(select.booking.peopleAmount);
     thisBooking.dom.hoursAmount = thisBooking.dom.wrapper.querySelector(select.booking.hoursAmount);
+
+    thisBooking.dom.datePicker = thisBooking.dom.wrapper.querySelector(select.widgets.datePicker.wrapper);
+    thisBooking.dom.hourPicker = thisBooking.dom.wrapper.querySelector(select.widgets.hourPicker.wrapper);
   }
 
   initWidgets() {
@@ -27,6 +33,10 @@ class Booking {
 
     thisBooking.peopleAmountWidget = new AmountWidget(thisBooking.dom.peopleAmount);
     thisBooking.hoursAmountWidget = new AmountWidget(thisBooking.dom.hoursAmount);
+
+    thisBooking.datePicker = new DatePicker(thisBooking.dom.datePicker);
+    thisBooking.hourPicker = new HourPicker(thisBooking.dom.hourPicker);
+
 
     thisBooking.dom.peopleAmount.addEventListener('updated', function() {
       // Here I will add logic to be executed after changing the number of people
